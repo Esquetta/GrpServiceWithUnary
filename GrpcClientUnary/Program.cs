712aa388b1d5b657ed79d87c8fe2a4588e3ea477
Esquetta <esquetta@gmail.com>
2023-08-17 21:33:11 +0300

    Add project files.

---
+++ GrpcClientUnary/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using gRPCExample;
+
+namespace GrpcClientUnary
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                var channel = GrpcChannel.ForAddress("http://localhost:5105");
+                var messageClient = new Message.MessageClient(channel);
+                Console.WriteLine("Lütfen gönderilecek mesajı giriniz.");
+                var messageResponse = await messageClient.GetMessageAsync(new MessageRequest { Message = Console.ReadLine() });
+                await Console.Out.WriteLineAsync($"Cevap: "+messageResponse);
+            }
+        }
+    }
+}
---
+++ GrpcServiceUnary/Services/MessageService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using gRPCExample;
+using static gRPCExample.Message;
+
+namespace GrpcServiceUnary.Services
+{
+    public class MessageService : MessageBase
+    {
+        public async override Task<MessageResponse> GetMessage(MessageRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"Mesaj alınmıştır.");
+            Console.WriteLine("Gelen mesaj : ");
+            Console.WriteLine(request.Message);
+
+            return new MessageResponse { Message = "İstek Alındı ve İşlendi" };
+        }
+    }
+}
